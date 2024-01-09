e1290d886670e2170b41169cc056a965692af00e
Owais Shariff <osh.econnect@gmail>
2024-01-09 11:11:43 +0530

    Text Edit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,18 +6,24 @@ const About = () => {
         <section>
         <div class="flex flex-col justify-center items-center">
           <h1 class="mt-30 text-center text-5xl text-white font-bold drop-shadow-lg">
-            A crypto investment platform <br /> that invests in you
+           Inspired by the Unique Asian Palm Civet
           </h1>
-          <p class="mt-5 text-center text-xl text-white opacity-70">
-            We invest more resources than any other platrom in making sure great{" "}
-            <br /> support from real people is a click away, whenever you need
-            it.
+          <p class="mt-5 px-64 text-center text-xl text-white opacity-70">
+          This cat-like creature is behind the world's most luxurious coffee, Kopi Luwak, 
+          <wbr />
+          made from beans processed in its digestive system, creating a rare and exquisite flavor. 
+          <wbr />
+          Our project, symbolized by the tagline "The Cat with a Utility," reflects this blend of natural wonder and luxury. 
+          <wbr />
+          We invite you to join us in unveiling Civet to the world, a symbol of rarity, innovation, 
+          <wbr />
+          and the extraordinary journey from nature to blockchain luxury.
           </p>
           <button
             type="button"
             class="mt-5 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-xl px-6 py-4 text-center me-2 mb-2"
           >
-            Get Started
+            Buy Now!
           </button>
         </div>
       </section>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,21 +3,20 @@ import React from 'react'
 const Cards = () => {
   return (
         <section class="text-gray-600 body-font mt-[-100px]">
-        <div class="container px-24 py-24 mx-auto">
+        <div class="container px-5 lg:px-24 py-24 mx-auto">
           <div class="flex flex-wrap -m-4">
             <div class="p-4 lg:w-1/3">
-              <div class="flex items-center flex-col h-full bg-[#1a1b23] bg-opacity-75 px-8 pt-16 pb-24 rounded-xl overflow-hidden text-center relative">
+              <div class="flex items-center flex-col h-full bg-[#1a1b23] bg-opacity-75 px-8 py-10 rounded-xl overflow-hidden text-center relative">
                 <img
                   class="align-center justify-center"
                   src="/card-img1.png"
                   alt="Card Icon"
                 />
                 <h1 class="mt-3 title-font sm:text-2xl text-4xl font-bold text-white mb-3">
-                  Trade Desk
+                  1B Supply
                 </h1>
                 <p class="text-[#898CA9] leading-relaxed mb-3">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
+                Limited to 1 billion coins—ensuring scarcity and value stability for a secure and enduring investment in our Solana-powered cryptocurrency.
                 </p>
                 <a class="text-purple-500 inline-flex items-center">
                   Learn More
@@ -37,18 +36,17 @@ const Cards = () => {
               </div>
             </div>
             <div class="p-4 lg:w-1/3">
-              <div class="flex items-center flex-col h-full bg-[#1a1b23] bg-opacity-75 px-8 pt-16 pb-24 rounded-xl overflow-hidden text-center relative">
+              <div class="flex items-center flex-col h-full bg-[#1a1b23] bg-opacity-75 px-8 py-10 rounded-xl overflow-hidden text-center relative">
                 <img
                   class="align-center justify-center"
                   src="/card-img2.png"
                   alt="Card Icon"
                 />
                 <h1 class="mt-3 title-font sm:text-2xl text-4xl font-bold text-white mb-3">
-                  Coinflip ATMs
+                  On Solana
                 </h1>
                 <p class="text-[#898CA9] leading-relaxed mb-3">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
+                Built on the high-performance Solana blockchain—experience rapid transaction speeds and low fees, making our coin an ideal choice for decentralized applications.
                 </p>
                 <a class="text-purple-500 inline-flex items-center">
                   Learn More
@@ -68,18 +66,17 @@ const Cards = () => {
               </div>
             </div>
             <div class="p-4 lg:w-1/3">
-              <div class="flex items-center flex-col h-full bg-[#1a1b23] bg-opacity-75 px-8 pt-16 pb-24 rounded-xl overflow-hidden text-center relative">
+              <div class="flex items-center flex-col h-full bg-[#1a1b23] bg-opacity-75 px-8 py-10 rounded-xl overflow-hidden text-center relative">
                 <img
                   class="align-center justify-center"
                   src="/card-img3.png"
                   alt="Card Icon"
                 />
                 <h1 class="mt-3 title-font sm:text-2xl text-4xl font-bold text-white mb-3">
-                  Coinflip Wallet
+                  0 Tax
                 </h1>
                 <p class="text-[#898CA9] leading-relaxed mb-3">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
+                Seamless transactions with zero tax—enjoy cost-effective and efficient transfers on the lightning-fast Solana blockchain.
                 </p>
                 <a class="text-purple-500 inline-flex items-center">
                   Learn More
