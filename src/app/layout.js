cdf1e47d34360ec4ec894b2352f548a1936b19ba
Owais Shariff <osh.econnect@gmail>
2024-01-09 12:25:49 +0530

    Speed Insights and Analytics Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <Analytics/>
+      <SpeedInsights/>
+      </body>
     </html>
   )
+
 }
